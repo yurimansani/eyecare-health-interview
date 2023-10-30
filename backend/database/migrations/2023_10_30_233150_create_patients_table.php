3b56d4a447f40814e6ee30dbde7f6b0699a54ddf
Yuri Mansani Vieira <yurimv9@gmail.com>
2023-10-30 20:58:52 -0300

    create table patients

---
+++ backend/database/migrations/2023_10_30_233150_create_patients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->string('name');
+            $table->string('email');
+            $table->string('cpf',11);
+            $table->string('rg',20);
+            $table->date('birth_date');
+            $table->enum('gender', ['male', 'female']);
+            $table->string('phone',12);
+            $table->text('description');
+
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
