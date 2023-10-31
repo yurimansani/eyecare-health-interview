717673ba8ede4a9775cebdfe78fb051212d0538e
Yuri Mansani Vieira <yurimv9@gmail.com>
2023-10-31 10:12:56 -0300

    Adicionando rota provisoria

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PatientController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::resource('/api/patients', PatientController::class);
+
 
 Route::get('/', function () {
     return view('welcome');
