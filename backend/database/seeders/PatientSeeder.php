a5eb9afefdddd181a42a935bc3bfce5041b95322
Yuri Mansani Vieira <yurimv9@gmail.com>
2023-10-31 10:09:17 -0300

    Criando o seeder com itens iniciais

--- backend/database/seeders/PatientSeeder.php
+++ backend/database/seeders/PatientSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Patient;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,7 @@ class PatientSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        
+        Patient::factory(10)->create();
     }
 }
